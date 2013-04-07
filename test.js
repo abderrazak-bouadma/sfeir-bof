4705b04ccfd0a7f964001d086076684c8271e1fd
abderrazak <bouadma.abderrazak@gmail.com>
2013-04-07 19:35:13 +0200

    test.js deleted

--- test.js
+++
@@ -1,6 +0,0 @@
-var expect = require("assert")
-describe('JavaScript Testing Should Be Awesome', function(){
-	it('Should say hello to the destinated person', function(){
-		expect('hello Abderrazak', sayHello('Abderrazak'));
-	})
-})
