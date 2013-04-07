36502c7441613c83111effdcf4b93bbe9e3949c7
abderrazak <bouadma.abderrazak@gmail.com>
2013-04-07 13:19:56 +0200

    initial commit

---
+++ test.js
@@ -0,0 +1,6 @@
+var expect = require("assert")
+describe('JavaScript Testing Should Be Awesome', function(){
+	it('Should say hello to the destinated person', function(){
+		expect('hello Abderrazak', sayHello('Abderrazak'));
+	})
+})
