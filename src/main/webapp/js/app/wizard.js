4588af77d267745d3eddf8d5822a97d18e9cc6c3
abderrazak <bouadma.abderrazak@gmail.com>
2013-04-15 22:09:57 +0200

    diffrent fix

--- src/main/webapp/js/app/wizard.js
+++ src/main/webapp/js/app/wizard.js
@@ -5,7 +5,7 @@
  * Time: 1:19 PM
  * To change this template use File | Settings | File Templates.
  */
-var wizardApp = angular.module('wizardApp',[]);
+var wizardApp = angular.module('wizardApp',['ngCookies']);
 
 //
 // routers
@@ -30,11 +30,47 @@ wizardApp.config(function($routeProvider){
 
 //
 // controllers
-wizardApp.controller('LoginCtrl',function($scope, $http) {
+wizardApp.controller('LoginCtrl',function($scope, $http, $cookieStore) {
     $scope.login = function(user) {
-        console.log('authenticating ' + user.email + ' ...')
+
+        var who;
+        console.log('Is there an authenticated user ? ' + $cookieStore.get('currentUser')==null?'No user connected':'current user is ' + $cookieStore.get('currentUser').email)
+        console.log('trying to authenticate ' + user.email + ' ...')
+
+        if ($cookieStore.get('currentUser')!=null) {
+            who = $cookieStore.get('currentUser')
+        } else {
+            who  =user
+        }
+
+        if (authenticate($http,who)) {
+            alert('Welcome ' + who.email)
+        } else {
+            alert('Authentication failed !')
+        }
     }
 })
+
+//
+var authenticate = function($http, user) {
+    $http.get('/json/users.json').success(function(data) {
+        var authenticatedUser;
+        for(var i=0; i<data.users.length; i++) {
+            if (user.email == data.users[i].email && user.password == data.users[i].password) {
+                authenticatedUser = data.users[i]
+                break
+            }
+        }
+        if (authenticatedUser != null) {
+            console.log('User ' + authenticatedUser.email + ' has been authenticated')
+            return true
+        } else {
+            console.log('Authentication failed !')
+            return false
+        }
+    })
+}
+
 
 //
 wizardApp.controller('WizardCtrl', function($scope){
