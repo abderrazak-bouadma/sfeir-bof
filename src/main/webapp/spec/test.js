ee2da51227499692e2e83afc6e810add521e4137
abderrazak <bouadma.abderrazak@gmail.com>
2013-04-19 10:55:53 +0200

    added an id to conference table div

--- src/main/webapp/spec/test.js
+++ src/main/webapp/spec/test.js
@@ -1,26 +1,46 @@
 
 //
-var baseUrl = 'http://sfeir-bof-organizer.appspot.com/'
+var baseUrl = 'http://sfeir-bof-organizer.appspot.com/#/'
+var localhostBaseUrl = 'http://localhost:8080/#/'
 
 //
 var Browser = require("zombie")
 var assert = require("assert")
+var should = require('chai').should()
 
 //
-describe('Wizards Test Suite', function() {
-	it('Should test@sfeir.com log into app and stored within a cookie', function() {
-		browser = new Browser()
-		browser.visit(baseUrl, function() {
-			var uEmail = 'test@sfeir.com'
-			var uPassword = 'test'
-			browser.
-				fill("#uEmail", uEmail).
-				fill("#uPassword", uPassword).
-				pressButton("Sign in", function() {
-					assert.ok(browser.success)
-					assert.ok(browser.query("conferenceTable"))
-					browser.close()
-				})
-		})
-	})
-})
+describe('Wizards Test Suite', function () {
+    it('Should test@sfeir.com log into app', function () {
+        var browser = new Browser()
+        console.log(browser)
+        browser.visit(localhostBaseUrl, function () {
+            var uEmail = 'test@sfeir.cAbderrazakom'
+            var uPassword = 'test'
+            browser.
+                fill('uEmail', uEmail).
+                fill('uPassword', uPassword).
+                pressButton("Sign in", function () {
+                    var ct = browser.query('conferenceTable')
+                    browser.success.should.equals(200)
+                    ct.should.exist
+                })
+        })
+    })
+
+    it('Should store user in a cookie', function() {
+        var browser = new Browser()
+        browser.visit(baseUrl, function () {
+            var uEmail = 'test@sfeir.com'
+            var uPassword = 'test'
+            browser.
+                fill('uEmail', uEmail).
+                fill('uPassword', uPassword).
+                pressButton("Sign in", function () {
+                    browser.success.should.equals(200)
+                    //browser.cookies.get('username').equals('Abderrazak')
+                })
+        })
+
+        browser.close()
+    })
+});
